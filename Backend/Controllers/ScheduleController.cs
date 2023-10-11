2af48f098ad457266ae4287fb1fed8ab0c358113
WildWolf <magnus1997.mho@gmail.com>
2023-10-11 09:17:06 +0200

    correct up schedule structure over requests

--- Backend/Controllers/ScheduleController.cs
+++ Backend/Controllers/ScheduleController.cs
@@ -5,10 +5,14 @@
 public class ScheduleController : ControllerBase
 {
     private readonly Database _database;
+    private readonly DiscordConnection _discord;
+    private readonly IHubContext _hub;
 
-    public ScheduleController(Database database)
+    public ScheduleController(Database database, DiscordConnection discord, IHubContext hub)
     {
         _database = database;
+        _discord = discord;
+        _hub = hub;
     }
 
     [HttpGet]
@@ -16,6 +20,45 @@ public class ScheduleController : ControllerBase
     public async Task<IActionResult> GetSchedule()
     {
         var schedules = await _database.Schedules.ToListAsync();
-        return Ok(schedules);
+        return Ok(schedules.Select(t => ScheduleHttp.FromSchedule(t, _discord)));
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public async Task<IActionResult> AddSchedule([FromBody] Schedule schedule)
+    {
+
+        await _database.Schedules.AddAsync(schedule);
+        await _database.SaveChangesAsync();
+        await _hub.Clients.All.SendAsync("ScheduleAdded", ScheduleHttp.FromSchedule(schedule, _discord));
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("update")]
+    public async Task<IActionResult> UpdateSchedule([FromBody] Schedule schedule)
+    {
+        _database.Schedules.Update(schedule);
+        await _database.SaveChangesAsync();
+        await _hub.Clients.All.SendAsync("ScheduleUpdated", ScheduleHttp.FromSchedule(schedule, _discord));
+        return Ok();
+    }
+
+    [HttpDelete]
+    [Route("delete")]
+    public async Task<IActionResult> DeleteSchedule([FromBody] Schedule schedule)
+    {
+        _database.Schedules.Remove(schedule);
+        await _database.SaveChangesAsync();
+        await _hub.Clients.All.SendAsync("ScheduleDeleted", schedule.Id);
+        return Ok();
+    }
+}
+
+public record ScheduleHttp(Guid Id, string Name, ulong HostId, string HostName, TimeSpan Duration, DateTime At)
+{
+    public static ScheduleHttp FromSchedule(Schedule schedule, DiscordConnection discord)
+    {
+        return new ScheduleHttp(schedule.Id, schedule.Name, schedule.HostId, discord.Guild!.GetUser(schedule.HostId).DisplayName, schedule.Duration, schedule.At);
     }
 }
